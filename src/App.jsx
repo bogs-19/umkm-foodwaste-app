48b48f320e58cb2c5e0be8da015aeec569b275ff
radhitama19 <baguspratama5000@gmail.com>
2026-06-23 13:14:24 +0700

    feat: inisialisasi struktur awal folder React Vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-green-600">
+        Aplikasi Food Waste UMKM Ready!
+      </h1>
+    </div>
+  )
+}
+
+export default App
